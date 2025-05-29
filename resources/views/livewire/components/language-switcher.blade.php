a60ce6dd017f5124df9c2ff94a284b129e9b0a3e
Naser Man <tgza54@gmail.com>
2025-05-29 20:15:34 +0300

    تحسنات الاداء

--- resources/views/livewire/components/language-switcher.blade.php
+++ resources/views/livewire/components/language-switcher.blade.php
@@ -5,10 +5,10 @@
      app()->setLocale($user->language); //تعين اللغة بناء على المستخدم
      session(['locale' => $user->language]); // تخزين اللغة في الجلسة
     @endphp
-    <div class=" relative inline-block text-left">
-        <select wire:model.lazy="language" class=" p-2 rounded text-white bg-blue-600" id="">
-            <option class=" hover:bg-blue-600" value="en">{{ __('English') }}</option>
-            <option class=" hover:bg-blue-600" value="ha">{{__('Hausa') }}</option>
+    <div x-data="{ lang: 'en'}" class=" relative inline-block text-left">
+        <select x-model="lang" wire:model.lazy="language" class=" p-2 rounded text-white bg-blue-600" id="">
+            <option class="" value="en">{{ __('English') }}</option>
+            <option class="" value="ha">{{__('Hausa') }}</option>
         </select>
-    </div>
+    </divء>
 </div>
