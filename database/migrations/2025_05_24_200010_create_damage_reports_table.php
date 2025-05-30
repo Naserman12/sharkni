722acd2176daccc2ddf1cd5b8b5f71a497c395b5
Naser Man <tgza54@gmail.com>
2025-05-31 00:14:13 +0300

    تحسينات الاداء

--- database/migrations/2025_05_24_200010_create_damage_reports_table.php
+++ database/migrations/2025_05_24_200010_create_damage_reports_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('damage_reports', function (Blueprint $table) {
             $table->id();
-            
             $table->foreignId('rental_id')->constrained()->cascadeOnDelete();
             $table->text('description')->nullable();
             $table->json('image_paths')->nullable();
