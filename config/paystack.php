789a51364d2152711e124513307b176e98553463
Naser Man <tgza54@gmail.com>
2025-06-06 03:41:40 +0300

    خيارات الدفع متوفرة الان مع تسينات اللغة new update

--- config/paystack.php
+++ config/paystack.php
@@ -15,25 +15,25 @@ return [
      * Public Key From Paystack Dashboard
      *
      */
-    'publicKey' => env('PAYSTACK_PUBLIC_KEY', 'pk_test_a1afe3532a741cb7fd47a4263172134110661c01'),
+    'publicKey' => getenv('PAYSTACK_PUBLIC_KEY', 'pk_test_a1afe3532a741cb7fd47a4263172134110661c01'),
 
     /**
      * Secret Key From Paystack Dashboard
      *
      */
-    'secretKey' => env('PAYSTACK_SECRET_KEY', 'sk_test_6103e5e0e2ec834940ef8a4f8316080977de01bc'),
+    'secretKey' => getenv('PAYSTACK_SECRET_KEY', 'sk_test_6103e5e0e2ec834940ef8a4f8316080977de01bc'),
 
     /**
      * Paystack Payment URL
      *
      */
-    'paymentUrl' => env('PAYSTACK_PAYMENT_URL', 'https://api.paystack.co'),
+    'paymentUrl' => getenv('PAYSTACK_PAYMENT_URL', 'https://api.paystack.co'),
 
     /**
      * Optional email address of the merchant
      *
      */
-    'merchantEmail' => env('MERCHANT_EMAIL'),
+    'merchantEmail' => getenv('MERCHANT_EMAIL'),
     'verify_ssl'=> false,
 
 
