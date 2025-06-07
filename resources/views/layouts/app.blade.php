9d251fadee5b7683bdcce1e9312eea9e41fb62b2
Naser Man <tgza54@gmail.com>
2025-06-07 04:18:26 +0300

    Style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
     </head>
-    <body class="font-sans antialiased bg-gradient-to-r from-gray-950 via-purple-500 to-pink-500">
+    <body class="font-sans antialiased bg-gradient-to-r from-red-500 via-orange-500 to-yellow-400 ">
             @php
             if(session()->has('language')){
             app()->setLocale(session('language'));
@@ -22,7 +22,7 @@
             @endphp
         <div>
         <!-- navbar -->
-         <nav class=" bg-gradient-to-r from-gray-500 via-blue-500 to-red-300 p-4 ">
+         <nav class=" bg-gradient-to-r from-red-300 via-orange-300 to-yellow-200 p-4 ">
              <div class=" container mx-auto flex justify-between items-center">
                  <a href="{{ route('dashboard') }}" class=" text-xl font-bold">{{ config('app.name', 'Sharkni') }}</a>
                  <div class="">
@@ -31,27 +31,27 @@
                     <i class=" fas fa-bars text-2xl"></i>
                  </button>
                  <!-- Notifications -->
-                 @auth
-                     @livewire('pages.notifications.notifications-list')
-                 @endauth
                  <div id="menu" class=" hidden md:flex md:items-center md:space-x-4 flex-col md:flex-row absolute md:static top-16 left-0 w-full md:w-auto bg-blue-200  md:bg-transparent p-4 md:p-0 shadow-md md:shadow-none ">
-                 <a href="{{ route('tools.index') }}" class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 {{ request()->routeIs('tools.index') ? ' bg-gray-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'Kayan Aiki' : 'Tools'  }}</a>
-                 <a href="{{ route('tools.add') }}"   class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 {{ request()->routeIs('tools.add') ? ' bg-gray-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'qrar kayan Aiki' : 'Add Tool'  }}</a>
-                 <a href="{{ route('rentals.index') }}"   class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 {{ request()->routeIs('rentals.index') ? ' bg-gray-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'Neman Aro' : 'Rentals'  }}</a>
-                 <a href="{{ route('damage.report') }}"   class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 {{ request()->routeIs('damage.report') ? ' bg-gray-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'Rahoton Lalacewa' : 'Report Damage'  }}</a>
-                 <a href="{{ route('categories.add') }}"   class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 {{ request()->routeIs('categories.add') ? ' bg-gray-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'Qara Nau\'i ' : 'Add Category'  }}</a>
-                 <a href="{{ route('profile') }}"     class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 {{ request()->routeIs('profile') ? ' bg-gray-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'Bayanan Mutun' : 'Profile'  }}</a>
-                 <a href="{{ route('logout') }}"      class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 bg-red-500 {{ request()->routeIs('logout') ? ' bg-red-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'Fita' : 'Logout '   }}</a>
-                 <div class="mt-2 md:mt-0">
-                    @livewire('components.language-switcher')
-                 </div>
+                     <a href="{{ route('tools.index') }}" class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 {{ request()->routeIs('tools.index') ? ' bg-gray-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'Kayan Aiki' : 'Tools'  }}</a>
+                     <a href="{{ route('tools.add') }}"   class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 {{ request()->routeIs('tools.add') ? ' bg-gray-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'qrar kayan Aiki' : 'Add Tool'  }}</a>
+                     <a href="{{ route('rentals.index') }}"   class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 {{ request()->routeIs('rentals.index') ? ' bg-gray-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'Neman Aro' : 'Rentals'  }}</a>
+                     <a href="{{ route('damage.report') }}"   class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 {{ request()->routeIs('damage.report') ? ' bg-gray-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'Rahoton Lalacewa' : 'Report Damage'  }}</a>
+                     <a href="{{ route('categories.add') }}"   class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 {{ request()->routeIs('categories.add') ? ' bg-gray-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'Qara Nau\'i ' : 'Add Category'  }}</a>
+                     <a href="{{ route('profile') }}"     class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 {{ request()->routeIs('profile') ? ' bg-gray-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'Bayanan Mutun' : 'Profile'  }}</a>
+                     <a href="{{ route('logout') }}"      class=" block md:inline-block py-2 md:py-0 hover:underline text-center md:text-left mr-4 bg-red-500 {{ request()->routeIs('logout') ? ' bg-red-500 p-2 rounded' : '' }}">{{ app()->getLocale() == 'ha' ? 'Fita' : 'Logout '   }}</a>
+                     <div class="mt-2 md:mt-0">
+                         @livewire('components.language-switcher')
+                     </div>
+                     @auth
+                      @livewire('pages.notifications.notifications-list')
+                     @endauth
                  </div>
                  </div>
              </div>
          </nav>
         <!-- Navbar End -->
             <!-- Page Content -->
-            <main class="min-h-screen bg-gradient-to-r from-gray-950 via-purple-500 to-pink-500">
+            <main class="min-h-screen bg-gradient-to-r from-red-400 via-orange-300 to-yellow-200">
                 
                 {{ $slot }}
             </main>
