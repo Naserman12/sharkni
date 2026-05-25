f9f073bb6b6074c819c9794dde567bd177e2e0e1
Naser Man <tgza54@gmail.com>
2026-05-25 10:43:42 +0300

    Ready to Railway npm 010

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator ;
+use Illuminate\Support\Facades\URL;
 use App\Services\PaystackService;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::defaultView('vendor.pagination.tailwind');
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
+    }
     }
 
     public function register()
